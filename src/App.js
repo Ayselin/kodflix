13ce322b7be6963a174195a31334f270dfae5252
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-01-07 15:10:37 +0000

    favorite cover photo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import starborn from './astarborn.jpg'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Welcome to Kodflix :) </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+     <img src ={starborn} alt ='Favorite film cover' /> 
       </div>
     );
   }
