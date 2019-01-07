a3270a7f944d69fa62f7367698645c4dfb39ab4e
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-01-07 16:23:49 +0000

    Add 6 TV show titles #3

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-     <img src ={starborn} alt ='Favorite film cover' /> 
-     <br/><br/><br/>
-     <div className ='container'>
-     <div className ='titles'>The Good Fight</div>
-     <div className ='titles'>One Day at a Time</div>
-     <div className ='titles' >Bodyguard</div>
-     </div>
-     <div className ='container'>
-     <div className ='titles'>This Country</div>
-     <div className ='titles'>Doctor Who</div>
-     <div className ='titles'>Trust</div>
-     </div>
-     </div>
-      
+        <img src={starborn} alt='Favorite film cover' />
+        <br /><br /><br />
+        <div className='container'>
+          <div className='titles'>The Good Fight</div>
+          <div className='titles'>One Day at a Time</div>
+          <div className='titles' >Bodyguard</div>
+        </div>
+        <div className='container'>
+          <div className='titles'>This Country</div>
+          <div className='titles'>Doctor Who</div>
+          <div className='titles'>Trust</div>
+        </div>
+      </div>
+
 
     );
   }
