04b5508b8b13714c0895191fff87587bc03f2c94
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-01-10 15:56:39 +0000

    Create a Gallery component to show the covers #9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import TvShow from './TvShow';
-import blindspot from './images/blindspot.jpg';
-import doctorwho from './images/doctorwho.jpg';
-import blackandwhite from './images/chb.jpg';
-import friendless from './images/friendless.jpg';
-import scorpion from './images/scorpion.jpg';
-import solve from './images/solve.jpg';
+import Gallery from './Gallery';
 import './App.css';
 
 
@@ -13,22 +7,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
         <br /><br /><br />
-        <div className='container'>
-          <TvShow name='Blind Spot' logo={blindspot} />
-          <TvShow name='Scorpion' logo={scorpion} />
-          <TvShow name='Black and White' logo={blackandwhite} />
-        </div>
-        <div className='container'>
-          <TvShow name='The friendless five' logo={friendless} />
-          <TvShow name='Doctor Who' logo={doctorwho} />
-          <TvShow name='Solve' logo={solve} />
-        </div>
+        <Gallery />
       </div>
     );
   }
 }
+
+
+
 
 
 export default App;
---
+++ src/Gallery.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TvShow from './TvShow';
+import blindspot from './images/blindspot.jpg';
+import doctorwho from './images/doctorwho.jpg';
+import blackandwhite from './images/chb.jpg';
+import friendless from './images/friendless.jpg';
+import scorpion from './images/scorpion.jpg';
+import solve from './images/solve.jpg';
+export default function Gallery() {
+    return (
+      <div>
+    <div className='container'>
+            <TvShow name='Blind Spot' logo={blindspot} />
+            <TvShow name='Scorpion' logo={scorpion} />
+            <TvShow name='Black and White' logo={blackandwhite} />
+          </div>
+          <div className='container'>
+            <TvShow name='The friendless five' logo={friendless} />
+            <TvShow name='Doctor Who' logo={doctorwho} />
+            <TvShow name='Solve' logo={solve} />
+          </div>
+        </div>
+    )
+  }
