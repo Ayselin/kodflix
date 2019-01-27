96bec2134d2a7b7dc98d29d0b0b282b30e6ed93b
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-01-27 17:13:35 +0000

    some changes in CSS

--- src/Frontend/TvShow.js
+++ src/Frontend/TvShow.js
@@ -6,7 +6,7 @@ export default function TvShow(props) {
     <Link to={`/${props.id}`} className='titles'>
       <img className='movedimg' src={props.logo} alt={`${props.name} logo`} />
       <div className='overlay'>
-        <h1>{props.name}</h1>
+        <h1 className='name'>{props.name}</h1>
       </div>
     </Link>
   );
